28c6c7e97ec10af2fb3557bc2eb5d3ecb175479b
Craig Scott <craigtscottjr@gmail.com>
2016-11-17 22:40:22 -0800

    got remove function working

--- w1d4/map.rb
+++ w1d4/map.rb
@@ -1,33 +1,44 @@
+require 'byebug'
 class Map
+
   def initialize
     @mvar = []
+  end
 
-      def assign(key, val)
-        if key_exist(key)
-          remove(key)
-          assign(key,val)
-        else
-          @mvar << [key,val]
-        end
-      end
+  def assign(key, val)
+    if key_exist(key)
+      remove(key)
+      assign(key,val)
+    else
+      @mvar << [key,val]
+    end
+  end
 
-      def key_exist(key)
-        @mvar.any? { |el| el[0] == key }
-      end
+  def key_exist(key)
+    @mvar.any? { |el| el[0] == key }
+  end
 
-      def remove(key)
-        @mvar.map { |el| el unles el == key }
-      end
+  def remove(key)
+    # debugger
+    @mvar.each_index do |idx|
+      @mvar.slice!(idx) if @mvar[idx][0] == key
+    end
+  end
 
-      def lookup(key)
-        @mvar.each_index do |idx|
-          @mvar[idx][1] if @mvar[idx][0] == key
-        end
-      end
+  def lookup(key)
+    # debugger
+    @mvar.each_index do |idx|
+    puts  @mvar[idx][1] if @mvar[idx][0] == key
+    end
+  end
 
-      
+
 
 end
 
-
+# a = Map.new
+# a.assign('a',1)
+# a.assign('b',2)
+# a.assign('c',3)
+# a.assign('d',4)
 # assign(key, value), lookup(key), remove(key), show
