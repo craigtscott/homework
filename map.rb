b8d5eb970b0bad073a2ebf9d75efeb4edadf1e66
Craig Scott <craigtscottjr@gmail.com>
2016-11-17 21:58:54 -0800

    added key_exist

--- map.rb
+++ map.rb
@@ -1,9 +1,21 @@
 class Map
   def initialize
     @mvar = []
-  end
 
+      def assign(key, val)
+        if key_exist(key)
+          remove(key)
+          assign(key,val)
+        else
+          @mvar << [key,val]
+        end
+      end
 
+      def key_exist(key)
+        @mvar.any? { |el| el[0] == key }
+      end
+
+    
 end
 
 
