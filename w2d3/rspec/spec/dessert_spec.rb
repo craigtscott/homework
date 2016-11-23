09caa14b511718995dc5907be79a177b9d97ba5b
Craig Scott <craigtscottjr@gmail.com>
2016-11-22 21:57:10 -0800

    7 out of 10 passing

---
+++ w2d3/rspec/spec/dessert_spec.rb
@@ -0,0 +1,69 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  let(:cookie) { Dessert.new('cookie', 100, chef) }
+
+  describe "#initialize" do
+    it "sets a type" do
+    expect(cookie.type).to eq('cookie')
+    end
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(100)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new("pie", "much", chef)}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+    expect(cookie.ingredients).to_not include('eggs')
+    cookie.add_ingredient('eggs')
+    expect(cookie.ingredients).to include('eggs')
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate chips", "flour", "egg", "sugar", "butter"]
+
+      ingredients.each do |ingredient|
+        cookie.add_ingredient(ingredient)
+      end
+
+      expect(cookie.ingredients).to eq(ingredients)
+      cookie.mix!
+      expect(cookie.ingredients).not_to eq(ingredients)
+      expect(cookie.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      expect(cookie.quantity).to eq(100)
+      cookie.eat(58)
+      expect(cookie.quantity).to eq(42)
+    end
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
