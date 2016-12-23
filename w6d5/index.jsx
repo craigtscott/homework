ae5d8babedfcd1559152d98b75e6c89b1fa7d2e9
Craig Scott <craigtscottjr@gmail.com>
2016-12-23 09:16:48 -0800

    calculator app

--- w6d5/index.jsx
+++ w6d5/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Congrats from './congrats';
+// import Congrats from './congrats';
+import Calculator from './calculator.jsx';
 
 document.addEventListener("DOMContentLoaded", () => {
 	const root = document.getElementById("root");
